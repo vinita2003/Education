974d35e2f32f23bd4da2b572cb5cfb0cf04003e8
Vinitashah83 <Vinitashah156@gmail.com>
2023-06-25 09:04:13 +0530

    second  commit

---
+++ index.js
@@ -0,0 +1,14 @@
+alert('hyy');
+
+burger = document.querySelector(".burger");
+navbar = document.querySelector(".navbar");
+rightNav = document.querySelector(".rightNav");
+navList = document.querySelector(".nav-list");
+
+burger.addEventListener('click', ()=>{
+    navList.classList.toggle("v-class-resp");
+    rightNav.classList.toggle("v-class-resp");
+
+    navbar.classList.toggle("h-nav-resp");
+
+});
